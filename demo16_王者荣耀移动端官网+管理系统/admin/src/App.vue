51b39390229eb18f518a64d9271fd81b790b1300
will1123 <will19961123@gmail.com>
2019-11-05 16:04:55 +0800

    王者荣耀admin搭建

---
+++ demo16_王者荣耀移动端官网+管理系统/admin/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    const item = {
+      date: "2016-05-02",
+      name: "王小虎",
+      address: "上海市普陀区金沙江路 1518 弄"
+    };
+    return {
+      tableData: Array(20).fill(item)
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+</style>
---
+++ demo16_王者荣耀移动端官网+管理系统/admin/src/README.md
@@ -0,0 +1,3 @@
+#### vue create admin 
+#### vue add router  不用history模式
+#### vue add element
