709332e7268e4fc116d05113a93737ebd862892f
will1123 <will19961123@gmail.com>
2019-11-09 14:30:11 +0800

    主页刚刚部分完成

--- demo16_王者荣耀移动端官网+管理系统/web/src/views/Main.vue
+++ demo16_王者荣耀移动端官网+管理系统/web/src/views/Main.vue
@@ -2,14 +2,24 @@
   <div>
     <div class="topbar bg-black py-2 px-3 d-flex ai-center">
       <img src="../assets/logo.png" height="30" alt />
-      <div class="px-2">
+      <div class="px-2 flex-1">
         <div class="text-white">王者荣耀</div>
         <div class="text-grey-1 fs-xxs">团队成就更多</div>
       </div>
       <button class="btn bg-primary text-black">立即下载</button>
     </div>
-    <div class="nav">
-      <h1>首页上方橘黄色的跳转菜单</h1>
+    <div class="bg-primary pb-2 pt-3">
+      <div class="nav d-flex text-white jc-around pb-1">
+        <div class="nav-item active">
+          <router-link class="nav-link" tag="div" to="/">首页</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link class="nav-link" tag="div" to="/">攻略中心</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link class="nav-link" tag="div" to="/">赛事中心</router-link>
+        </div>
+      </div>
     </div>
 
     <router-view></router-view>
